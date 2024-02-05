fef9a3989b5a4b8fd17e42828855cdcd3fc34eae
Marissa Sun <sunmarissa@MacBook-Air-9.local>
2024-02-04 23:01:20 -0500

    finished code

--- SearchPrefix.rb
+++ SearchPrefix.rb
@@ -1,7 +1,12 @@
 def searchPrefix(arr,s)
-
-end
-
-print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"ab"))
-
-print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"b"))
+    newArray = Array.new
+    for i in 0..arr.length-1
+      if arr[i].start_with?(s)
+        newArray.push(arr[i])
+      end
+    end
+    print newArray
+  end
+  
+  puts(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"ab"))
+  puts(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"b"))
